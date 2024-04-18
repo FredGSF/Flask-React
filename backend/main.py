9381ed4a2f5f38ce5bc7c96eeb6dcad035dc35f9
FredGSF <99353119+FredGSF@users.noreply.github.com>
2024-04-18 18:30:50 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,20 @@
 from flask import request, jsonify
 from config import app, db
 from models import Contact
+
+
+## Decorator
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    # Convert python to json
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+
+# Run code if in main
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
